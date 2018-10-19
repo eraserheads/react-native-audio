697d695d22dd56eed82bd7c90f09af8f945f10b1
Enrico Garia <enricogaria@gmail.com>
2018-10-19 12:13:17 +0200

    fix warning requiresMainQueueSetup

--- ios/AudioRecorderManager.m
+++ ios/AudioRecorderManager.m
@@ -54,7 +54,7 @@ RCT_EXPORT_MODULE();
           [_audioRecorder updateMeters];
           float _currentMetering = [_audioRecorder averagePowerForChannel: 0];
           [body setObject:[NSNumber numberWithFloat:_currentMetering] forKey:@"currentMetering"];
-   
+
           float _currentPeakMetering = [_audioRecorder peakPowerForChannel:0];
           [body setObject:[NSNumber numberWithFloat:_currentPeakMetering] forKey:@"currentPeakMetering"];
       }
@@ -62,6 +62,10 @@ RCT_EXPORT_MODULE();
 
     _prevProgressUpdateTime = [NSDate date];
   }
+}
+
++ (BOOL) requiresMainQueueSetup {
+  return YES;
 }
 
 - (void)stopProgressTimer {
@@ -86,7 +90,7 @@ RCT_EXPORT_MODULE();
   }
     uint64_t audioFileSize = 0;
     audioFileSize = [[[NSFileManager defaultManager] attributesOfItemAtPath:[_audioFileURL path] error:nil] fileSize];
-  
+
   [self.bridge.eventDispatcher sendAppEventWithName:AudioRecorderEventFinished body:@{
       @"base64":base64,
       @"duration":@(_currentTime),
@@ -94,7 +98,7 @@ RCT_EXPORT_MODULE();
       @"audioFileURL": [_audioFileURL absoluteString],
       @"audioFileSize": @(audioFileSize)
     }];
-    
+
     // This will resume the music/audio file that was playing before the recording started
     // Without this piece of code, the music/audio will just be stopped
     NSError *error;
@@ -183,7 +187,7 @@ RCT_EXPORT_METHOD(prepareRecordingAtPath:(NSString *)path sampleRate:(float)samp
     }
   }
 
-    
+
   // Set sample rate from options
   _audioSampleRate = [NSNumber numberWithFloat:sampleRate];
 
